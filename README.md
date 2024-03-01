faa946cd1fdacd976d963c3834c9740cbe9251e9
Andrew H Farkas <59749066+AndrewHFarkas@users.noreply.github.com>
2024-03-01 13:47:49 -0500

    Deoblique everything now
    Deobliquing the functional and distortion maps improves all output so that is now the default. Cleaned up some naming so it is more clear what is a path to a file or directory.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Gaborgen
 
-Currently in this repository we are building up an fMRI preprocessing pipeline for the Gaborgen study. Eventually this may evolve into analyses for the EEG and fMRI data from this study.
+Currently in this repository we are building up an fMRI preprocessing pipeline for the Gaborgen study. Eventually this may evolve into analyses for the EEG and fMRI data from this study. Base R is used so that there are no added dependencies besides AFNI and dcm2niix. It performs an initial regression with all stimuli to get nice quality control features which can be used for subsequent regressions and analyses.
 
 To recreate the analyses, data should be taken from the dropbox in the same format as it is kept there for example:
+
 GABORGEN24_101
+
 - fMRI
+
 - EEG
+
 - dat
 
+## 001fMRI_prepro.R
+This script does the initial preprocessing per participant. It imposes restrictions to force the user to have the data in the correct format and to prevent incorrect analyses. It also forces the user to put their data and local git directory paths at the top of the script. The script forces the use of the tcsh terminal which is recommended for AFNI analyses. The system2 R function is used to force tcsh terminal calls.
 
-
-The master script imposes restrictions by making sure that the data is in the correct format and that the tcsh terminal is available (which is recommended for AFNI analyses). The system2 R function is used to force tcsh terminal calls. It also forces the user to put their data and local git directory paths at the top of the script.
--- format_mri.R
+++ format_mri.R
@@ -1,5 +1,4 @@
-# Convert structural and functional images to nifti fomat, deoblique structural
-# functional should not need to be deobliqued
+# Convert files to NIFTI format and deobliques
 # If there are multiple of the same scan, it processes the last one recorded by
 # looking for the largest number at the end of folder
 for (participant_index in 1:length(participants_to_preprocess)) {
@@ -11,7 +10,7 @@ for (participant_index in 1:length(participants_to_preprocess)) {
     paste0(participant_directories[participant_index],'/fMRI'),
     pattern = 'T1_MPRAGE_SAG_P2_ISO', full.names = T)
   
-  current_structural_path <- 
+  current_structural_directory <- 
     current_participant_found_structural_directories[
       which.max(as.numeric(
         sub(".*?(\\d+)$", "\\1", 
@@ -21,7 +20,7 @@ for (participant_index in 1:length(participants_to_preprocess)) {
     paste0(participant_directories[participant_index],'/fMRI'),
     pattern = 'BOLD-EPI-CMRR-2S', full.names = T)
   
-  current_functional_path <- current_participant_found_functional_directories[
+  current_functional_directory <- current_participant_found_functional_directories[
     which.max(as.numeric(
       sub(".*?(\\d+)$", "\\1", 
           current_participant_found_functional_directories)))]
@@ -30,7 +29,7 @@ for (participant_index in 1:length(participants_to_preprocess)) {
     paste0(participant_directories[participant_index],'/fMRI'),
     pattern = 'CMMR-DISTMAP_AP', full.names = T)
   
-  current_blip_forward_path <- current_participant_found_blip_forward[
+  current_blip_forward_directory <- current_participant_found_blip_forward[
     which.max(as.numeric(
       sub(".*?(\\d+)$", "\\1", 
           current_participant_found_blip_forward)))]
@@ -39,7 +38,7 @@ for (participant_index in 1:length(participants_to_preprocess)) {
     paste0(participant_directories[participant_index],'/fMRI'),
     pattern = 'CMMR-DISTMAP_PA', full.names = T)
   
-  current_blip_reverse_path <- current_participant_found_blip_reverse[
+  current_blip_reverse_directory <- current_participant_found_blip_reverse[
     which.max(as.numeric(
       sub(".*?(\\d+)$", "\\1", 
           current_participant_found_blip_reverse)))]
@@ -49,78 +48,121 @@ for (participant_index in 1:length(participants_to_preprocess)) {
   # a good option on a mac
   
   # Structural to NIFTI
-  if (length(list.files(current_structural_path, pattern = 'nii$')) == 1 &
-      length(list.files(current_structural_path, pattern = 'json$')) == 1) {
+  if (length(list.files(current_structural_directory, pattern = 'nii$')) == 1 &
+      length(list.files(current_structural_directory, pattern = 'json$')) == 1) {
     
     print('Structural NIFTI file already appears to exist, skipping conversion')
     
-  } else if (length(list.files(current_structural_path, pattern = 'nii$')) > 1 &
-             length(list.files(current_structural_path, pattern = 'json$')) > 1) {
+  } else if (length(list.files(current_structural_directory, pattern = 'nii$')) > 1 &
+             length(list.files(current_structural_directory, pattern = 'json$')) > 1) {
     
-    stop('Multiple NIFTI files found in the current structural path. There should be 1 or 0.')
+    stop('Multiple NIFTI files found in the current structural directory. There should be 1 or 0.')
     
   } else {
     system2('tcsh', 
-            args = c('-c', shQuote(paste('dcm2niix', current_structural_path))))
+            args = c('-c', shQuote(paste('dcm2niix', current_structural_directory))))
   }
   
   
   # Functional to NIFTI
-  if (length(list.files(current_functional_path, pattern = 'nii$')) == 1 &
-      length(list.files(current_functional_path, pattern = 'json$')) == 1) {
+  if (length(list.files(current_functional_directory, pattern = 'nii$')) == 1 &
+      length(list.files(current_functional_directory, pattern = 'json$')) == 1) {
     
     print('Functional NIFTI file already appears to exist, skipping conversion')
     
-  } else if (length(list.files(current_functional_path, pattern = 'nii$')) > 1 &
-             length(list.files(current_functional_path, pattern = 'json$')) > 1) {
+  } else if (length(list.files(current_functional_directory, pattern = 'nii$')) > 1 &
+             length(list.files(current_functional_directory, pattern = 'json$')) > 1) {
     
-    stop('Multiple NIFTI files found in the current functional path. There should be 1 or 0.')
+    stop('Multiple NIFTI files found in the current functional directory. There should be 1 or 0.')
     
   }  else {
     system2('tcsh', 
-            args = c('-c', shQuote(paste('dcm2niix', current_functional_path))))
+            args = c('-c', shQuote(paste('dcm2niix', current_functional_directory))))
   }
-
+  
   # Blip distortion maps to NIFTI
-  if (length(list.files(current_blip_forward_path, pattern = 'nii$')) == 1 &
-      length(list.files(current_blip_forward_path, pattern = 'json$')) == 1) {
+  if (length(list.files(current_blip_forward_directory, pattern = 'nii$')) == 1 &
+      length(list.files(current_blip_forward_directory, pattern = 'json$')) == 1) {
     print('Blip forward NIFTI file already appears to exist, skipping conversion')
-  } else if (length(list.files(current_blip_forward_path, pattern = 'nii$')) > 1 &
-             length(list.files(current_blip_forward_path, pattern = 'json$')) > 1) {
-    stop('Multiple NIFTI files found in the current blip forward path. There should be 1 or 0.')
+  } else if (length(list.files(current_blip_forward_directory, pattern = 'nii$')) > 1 &
+             length(list.files(current_blip_forward_directory, pattern = 'json$')) > 1) {
+    stop('Multiple NIFTI files found in the current blip forward directory. There should be 1 or 0.')
   }  else {
     system2('tcsh', 
-            args = c('-c', shQuote(paste('dcm2niix', current_blip_forward_path))))
+            args = c('-c', shQuote(paste('dcm2niix', current_blip_forward_directory))))
   }
-
-  if (length(list.files(current_blip_reverse_path, pattern = 'nii$')) == 1 &
-      length(list.files(current_blip_reverse_path, pattern = 'json$')) == 1) {
+  
+  if (length(list.files(current_blip_reverse_directory, pattern = 'nii$')) == 1 &
+      length(list.files(current_blip_reverse_directory, pattern = 'json$')) == 1) {
     print('Blip reverse NIFTI file already appears to exist, skipping conversion')
-  } else if (length(list.files(current_blip_reverse_path, pattern = 'nii$')) > 1 &
-             length(list.files(current_blip_reverse_path, pattern = 'json$')) > 1) {
-    stop('Multiple NIFTI files found in the current blip reverse path. There should be 1 or 0.')
+  } else if (length(list.files(current_blip_reverse_directory, pattern = 'nii$')) > 1 &
+             length(list.files(current_blip_reverse_directory, pattern = 'json$')) > 1) {
+    stop('Multiple NIFTI files found in the current blip reverse directory. There should be 1 or 0.')
   }  else {
     system2('tcsh', 
-            args = c('-c', shQuote(paste('dcm2niix', current_blip_reverse_path))))
+            args = c('-c', shQuote(paste('dcm2niix', current_blip_reverse_directory))))
   }
   
   
   # Structural deobliqued
-  if (length(list.files(current_structural_path, pattern = '^ANAT_DEOB')) > 0) {
+  if (length(list.files(current_structural_directory, pattern = '^ANAT_DEOB')) > 0) {
     print('Deobliqued structural file already appears to exist, skipping conversion')
   } else {
-    
     # necessary so the obliqued data is written to the right place
-    setwd(current_structural_path)
-    
-    current_structural_NIFTI_path <- list.files(current_structural_path, 
+    setwd(current_structural_directory)
+    current_structural_NIFTI_path <- list.files(current_structural_directory, 
                                                 pattern = '.nii$', 
                                                 full.names = T)
     system2('tcsh', 
             args = c('-c', 
                      shQuote(paste('3dWarp -deoblique -prefix ANAT_DEOB', 
                                    current_structural_NIFTI_path))))
-    
-    setwd(local_git_directory)
   }
+  
+  # Functional deobliqued
+  if (length(list.files(current_functional_directory, pattern = '^FUNC_DEOB')) > 0) {
+    print('Deobliqued functional file already appears to exist, skipping conversion')
+  } else {
+    # necessary so the obliqued data is written to the right place
+    setwd(current_functional_directory)
+    current_functional_NIFTI_path <- list.files(current_functional_directory, 
+                                                pattern = '.nii$', 
+                                                full.names = T)
+    system2('tcsh', 
+            args = c('-c', 
+                     shQuote(paste('3dWarp -deoblique -prefix FUNC_DEOB', 
+                                   current_functional_NIFTI_path))))
+  }
+  
+  # Blip forward deobliqued
+  if (length(list.files(current_blip_forward_directory, pattern = '^BLIP_FOR_DEOB')) > 0) {
+    print('Deobliqued blip forward file already appears to exist, skipping conversion')
+  } else {
+    # necessary so the obliqued data is written to the right place
+    setwd(current_blip_forward_directory)
+    current_blip_forward_NIFTI_path <- list.files(current_blip_forward_directory, 
+                                                  pattern = '.nii$', 
+                                                  full.names = T)
+    system2('tcsh', 
+            args = c('-c', 
+                     shQuote(paste('3dWarp -deoblique -prefix BLIP_FOR_DEOB', 
+                                   current_blip_forward_NIFTI_path))))
+  }
+  
+  # Blip reverse deobliqued
+  if (length(list.files(current_blip_reverse_directory, pattern = '^BLIP_REV_DEOB')) > 0) {
+    print('Deobliqued blip forward file already appears to exist, skipping conversion')
+  } else {
+    # necessary so the obliqued data is written to the right place
+    setwd(current_blip_reverse_directory)
+    current_blip_reverse_NIFTI_path <- list.files(current_blip_reverse_directory, 
+                                                  pattern = '.nii$', 
+                                                  full.names = T)
+    system2('tcsh', 
+            args = c('-c', 
+                     shQuote(paste('3dWarp -deoblique -prefix BLIP_REV_DEOB', 
+                                   current_blip_reverse_NIFTI_path))))
+    
+  }
+  setwd(local_git_directory)
 }
