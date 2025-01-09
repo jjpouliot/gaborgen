aa57b25bd57145afd5e656328f644f84dd617f2d
Andrew H Farkas <59749066+AndrewHFarkas@users.noreply.github.com>
2025-01-09 14:34:27 -0500

    extracting single trials now works if the epoch is infront of the trigger onset

--- a003EEG_extract_single_trials.m
+++ a003EEG_extract_single_trials.m
@@ -16,11 +16,15 @@ addpath(genpath('/home/andrewf/Repositories/freqTag'), '-end');
 
 %% Load participant EEG, find good trials, extract 15Hz ssVEP
 
-participantIDs = [110,111,112,113,114,115,116,117,118,119,120,121,122,123,125,126,127,128,129,130];
-epochMs = [0 2000];
+% participantIDs = [110,111,112,113,114,115,116,117,118,119,120,121,122,123,125,126,127,128,129,130];
+% participantIDs = [131,132,133,134,135,136,139,140,141,142,143];
+participantIDs = [110,111,112,113,114,115,116,117,118,119,120,121,122,123,125,126,...
+                            127,128,129,130,131,132,133,134,135,136,139,140,141,142,143];
+epochMs = [500 2000];
 sampleRateHz = 500;
 resampledRateHz = 600; % Check if this makes sense
-epochSamplePoints = 1:(epochMs(2)*(sampleRateHz/1000));
+postEpochedSamplePoints = 1:((epochMs(2) - epochMs(1))*(sampleRateHz/1000));
+% epochSamplePoints = 1:(epochMs(2)*(sampleRateHz/1000));
 rawDataPath = '/home/andrewf/Research_data/EEG/Gaborgen24_EEG_fMRI';
 startOfStimMarkerRegEx = '^S\s[1234]|^S121';
 
@@ -63,36 +67,77 @@ for participantIndex = 1:length(participantIDs)
         chanList = chanList(1,1,:);
 
         EEG = eeg_checkset(EEG);
-        EEG = pop_epoch(EEG, startOfStimMarkerRegEx, ...
+
+        % urevent has all possible event triggers sent, this will have all
+        % the stimuli triggers sent. The event field will have events that
+        % are retained after preprocessing. After epoching, even more will
+        % events will be lost; and importantly, if the epoch extends
+        % infront of the trigger, then the stimulus event will be lost. So
+        % the following code saves the index for the urevent and then uses
+        % the retained epoch indices to find the final trials kept.
+        ureventTable = struct2table(EEG.urevent);
+        eventTable = struct2table(EEG.event);
+
+        allPossibleEvents = {EEG.urevent.type};
+        allPossibleEventIndices = 1:length(allPossibleEvents);
+
+        allEventsIndicesLeft = {EEG.event.urevent};
+        allEventsTypesLeft = {EEG.event.type};
+        allEventsIndicesLeft = {EEG.event.urevent};
+        
+        % Use regular expressions to find stimuli events
+        matchIdx = ~cellfun('isempty', regexp(allPossibleEvents, startOfStimMarkerRegEx, 'once'));
+        allPossibleStimuliIndices = allPossibleEventIndices(matchIdx);
+
+        allStimuliTriggersSent = allPossibleEvents(matchIdx);
+
+        trialNumber = 1:length(allStimuliTriggersSent);
+
+        matchIdx = ~cellfun('isempty', regexp(allEventsTypesLeft, startOfStimMarkerRegEx, 'once'));
+        currentStimuliIndicesLeft = allEventsIndicesLeft(matchIdx);
+
+
+        [EEG, acceptedEventIndices] = pop_epoch(EEG, startOfStimMarkerRegEx, ...
             epochMs ./ 1000, 'newname', 'segmented', 'epochinfo', 'yes');
         [~, EEG, ~] = pop_newset(ALLEEG, EEG, 1,'gui','off');
 
-        %% list the original trial indices from urevents for each trial category
-        eventTable = struct2table(EEG.event);
-        ureventTable = struct2table(EEG.urevent);
-        %cleanSegmentsBool = cell(1);
+        finalStimuliIndicesLeft = currentStimuliIndicesLeft(acceptedEventIndices);
+        finalStimuliIndicesLeft = cell2mat(finalStimuliIndicesLeft);
 
-        eventInd = [];
-        ureventInd = [];
-        for i = 1:length(ureventTable.type)
-            if regexp(ureventTable.type{i}, startOfStimMarkerRegEx, 'once')
-                ureventInd = [ureventInd, i];
-            end
-        end
-        for i = 1:length(eventTable.type)
-            if regexp(eventTable.type{i}, startOfStimMarkerRegEx, 'once')
-                eventInd = [eventInd, eventTable.urevent(i)];
-            end
-        end
-        cleanSegmentsBool = ismember(ureventInd, eventInd);
-
-        trialNumber = 1:length(ureventInd);
+        cleanSegmentsBool = ismember(allPossibleStimuliIndices, finalStimuliIndicesLeft);
 
         cleanTrialNumber = trialNumber(cleanSegmentsBool);
 
+
+%         %% list the original trial indices from urevents for each trial category
+%         eventTable = struct2table(EEG.event);
+%         ureventTable = struct2table(EEG.urevent);
+%         %cleanSegmentsBool = cell(1);
+% 
+%         eventInd = [];
+%         ureventInd = [];
+%         for i = 1:length(ureventTable.type)
+%             if regexp(ureventTable.type{i}, startOfStimMarkerRegEx, 'once')
+%                 ureventInd = [ureventInd, i];
+%             end
+%         end
+%         for i = 1:length(eventTable.type)
+%             if regexp(eventTable.type{i}, startOfStimMarkerRegEx, 'once')
+%                 eventInd = [eventInd, eventTable.urevent(i)];
+%             end
+%         end
+%         cleanSegmentsBool = ismember(ureventInd, eventInd);
+% 
+%         trialNumber = 1:length(ureventInd);
+% 
+%         cleanTrialNumber = trialNumber(cleanSegmentsBool);
+
         % sliding window post-cue - 15 Hz
+        % [trialamp,winmat3d,phasestabmat,trialSNR] = freqtag_slidewin(data, plotflag, bslvec, ssvepvec, foi, sampnew, fsamp, outname)
+        % bslvec and ssvepvec have to be the indices of the already
+        % extracted epoch.
         [trialamp15Hz, winmat3d15Hz, phasestabmat15Hz, trialSNR15Hz] = ...
-            freqtag_slidewin(EEG.data, 0, epochSamplePoints, epochSamplePoints, ...
+            freqtag_slidewin(EEG.data, 0, postEpochedSamplePoints, postEpochedSamplePoints, ...
             15, resampledRateHz, EEG.srate, 'whatever.txt');
 
 
@@ -101,9 +146,9 @@ for participantIndex = 1:length(participantIDs)
 
         % Save the raw and sliding window timeseries and FFT by trial so
         % that I know which trials are missing
-         mkdir([rawDataPath '/single_trial_timeseries_FFTs'])
+         mkdir([rawDataPath '/single_trial_timeseries_FFTs_CSD_500_2000ms'])
 
-         matSavePath = [rawDataPath '/single_trial_timeseries_FFTs/'];
+         matSavePath = [rawDataPath '/single_trial_timeseries_FFTs_CSD_500_2000ms/'];
 
         for trialIndex = 1:length(cleanTrialNumber)
             participantID = participantIDs(participantIndex);
@@ -119,7 +164,7 @@ for participantIndex = 1:length(participantIDs)
             rawTimeseriesName = ...
                 ['rawChanTime_participant' num2str(participantID) '_trial' ...
                 num2str(currentTrialNumber) '_condition'...
-                ureventTable.type{eventInd(trialIndex)} ...
+                ureventTable.type{finalStimuliIndicesLeft(trialIndex)} ...
                 '_sampleRate' num2str(EEG.srate) 'Hz.mat'];
 
             rawTimeseriesName = regexprep(rawTimeseriesName, '\s', '');
@@ -127,7 +172,7 @@ for participantIndex = 1:length(participantIDs)
             SlidingWindowTimeseriesName = ...
                 ['slidingWindowChanTime_participant' num2str(participantID) '_trial' ...
                 num2str(currentTrialNumber) '_condition'...
-                ureventTable.type{eventInd(trialIndex)} ...
+                ureventTable.type{finalStimuliIndicesLeft(trialIndex)} ...
                 '_sampleRate' num2str(resampledRateHz) 'Hz.mat'];
 
             SlidingWindowTimeseriesName = regexprep(SlidingWindowTimeseriesName, '\s', '');
@@ -150,7 +195,7 @@ for participantIndex = 1:length(participantIDs)
             rawFFTName = ...
                 ['rawChanFrequency_participant' num2str(participantID) '_trial' ...
                 num2str(currentTrialNumber) '_condition'...
-                ureventTable.type{eventInd(trialIndex)} ...
+                ureventTable.type{finalStimuliIndicesLeft(trialIndex)} ...
                 '_sampleRate' num2str(EEG.srate) 'Hz.mat'];
 
             rawFFTName = regexprep(rawFFTName, '\s', '');
@@ -158,7 +203,7 @@ for participantIndex = 1:length(participantIDs)
             SlidingWindowFFTName = ...
                 ['slidingWindow_ChanFrequency_participant' num2str(participantID) '_trial' ...
                 num2str(currentTrialNumber) '_condition'...
-                ureventTable.type{eventInd(trialIndex)} ...
+                ureventTable.type{finalStimuliIndicesLeft(trialIndex)} ...
                 '_sampleRate' num2str(resampledRateHz) 'Hz.mat'];
 
             SlidingWindowFFTName = regexprep(SlidingWindowFFTName, '\s', '');
