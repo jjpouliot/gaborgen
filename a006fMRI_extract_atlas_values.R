db69ac3bca28d12e72d28b4abd6d0e46d5058e76
Andrew H Farkas <59749066+AndrewHFarkas@users.noreply.github.com>
2025-04-09 16:04:39 -0400

    Update a006fMRI_extract_atlas_values.R

--- a006fMRI_extract_atlas_values.R
+++ a006fMRI_extract_atlas_values.R
@@ -6,58 +6,41 @@ full_design_matrix_paths <- list.files(path = data_location, pattern = 'X.nocens
 
 full_design_matrix_new_names <- sub(replacement = "_", pattern = "/", x = full_design_matrix_paths)
 
-file.copy(from = full_design_matrix_paths,to = paste0(where_to_copy, "/", full_design_matrix_new_names))
+file.copy(from = paste0(data_location, "/", full_design_matrix_paths), to = paste0(where_to_copy, "/", full_design_matrix_new_names))
 
 # copy censor info
 censor_info_paths <- list.files(path = data_location, pattern = 'combined_2.1D$', recursive = T)
 
-file.copy(from = censor_info_paths, to = paste0(where_to_copy, "/", censor_info_paths))
+file.copy(from = paste0(data_location, "/", censor_info_paths), to = paste0(where_to_copy, "/", basename(censor_info_paths)))
 
 
-## Add where your local git repository is ####
-known_gaborgen_local_gits <- c('~/Documents/GitHub/gaborgen',
-                               '/blue/akeil/andrew.farkas/gaborgen24_eeg_fmri/code_repository/gaborgen')
+# copy functional data
 
-## Add where your data is, it should be the same format and organization as it was in the dropbox ####
-known_data_locations <- c('~/research_data/gaborgen/raw_data',
-                          '/blue/akeil/andrew.farkas/gaborgen24_eeg_fmri/raw_data')
+processed_functional_paths <- list.files(path = data_location, pattern = "pb05", recursive = T)
 
-## Where the results should be saved
-# Andrew Farkas Mac 
-where_results_should_be_saved <- "/Users/andrewfarkas/research_data/gaborgen/results"
-# Andrew Farkas hipergator
-# where_results_should_be_saved <- "/blue/akeil/andrew.farkas/gaborgen24_eeg_fmri/results"
+file.copy(from = paste0(data_location, "/", processed_functional_paths), to = paste0(where_to_copy, "/", basename(processed_functional_paths)))
 
-if(!file.exists(where_results_should_be_saved)){
-  stop("Choose a results folder that exists")
-}
 
-existing_git_locations <- file.exists(known_gaborgen_local_gits)
+# calculate ROI times series per participant, just day 1 right now
 
-number_of_found_gits <- 0
+participants_to_process <- c(101:154)
 
-for(path_index in 1:length(known_gaborgen_local_gits)){
-  if (file.exists(known_gaborgen_local_gits[path_index])) {
-    number_of_found_gits <- number_of_found_gits + 1
-    local_git_directory <- known_gaborgen_local_gits[path_index]
+for (i in 1:length(participants_to_process)) {
+  setwd(where_to_copy) # go to ROI folder
+  
+  # run this terminal command, expect per participant, gets timeseries per ROI into text file
+  #   3dROIstats -mask HCPex_SUIT_func_mask.nii.gz -nzmean -median pb05.GABORGEN24_DAY1_149.r01.scale+tlrc > 149_roi_stats.txt
+  
+  if (participants_to_process[i] < 123) {
+    system2('tcsh', 
+            args = c('-c', shQuote(paste('3dROIstats -mask HCPex_SUIT_func_mask.nii.gz -nzmean -median pb05.GABORGEN24_', 
+                                         participants_to_process[i], ".r01.scale+tlrc > ", participants_to_process[i], "_roi_stats.txt",sep = ""))))
+    
+  } else {
+    system2('tcsh', 
+             args = c('-c', shQuote(paste('3dROIstats -mask HCPex_SUIT_func_mask.nii.gz -nzmean -median pb05.GABORGEN24_DAY1_', 
+                                    participants_to_process[i], ".r01.scale+tlrc > ", participants_to_process[i], "_roi_stats.txt",sep = ""))))
   }
 }
-
-if (number_of_found_gits == 0) {
-  stop('Add your data folder to known_data_locations vector above')
-}
-
-if (number_of_found_gits > 1) {
-  stop('Multiple data locations found on the current computer')
-}
-
-setwd(local_git_directory)
-
-
-### tests
-system2('tcsh', 
-        args = c('-c', shQuote(paste('dcm2niix', current_functional_directory))))
-
-
 
 
